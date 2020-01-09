639543d814cecea116582546c1158e374c561364
高波　由佳 <y-takanami@takanamiyukanoMacBook-Air.local>
2020-01-09 17:19:09 +0900

    [start]3-7

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,6 @@ Rails.application.routes.draw do
   get 'todolists' => 'todolists#index'
 
   get 'todolists/:id' => 'todolists#show', as: 'todolist'
+
+  get 'todolists/:id/edit' => 'todolists#edit',as:'edit_todolist'
 end
