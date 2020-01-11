e23bf0ca58ab8aa89c7059146b38e1de6174830f
高波　由佳 <y-takanami@takanamiyukanoMacBook-Air.local>
2020-01-11 16:00:36 +0900

    [finish]3-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'c1e663dda182a0dfa3535ef38e8ef0e155b71226facaf7e4dfc71f796739526dccc22718342aed51f06b4d0ce9a16a683ed95975c7a31bd4abdca99a0fcc9d14'
